76e9251b97b0d6d9fc73a2b23397487dc78493bc
GregRos <life.gregr@gmail.com>
2025-05-04 23:40:09 +0300

    Added vscode launcher, some fixes

---
+++ src/vscode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+static class Vscode {
+    static void Main(string[] args) {
+        var jArgs = string.Join(" ", args);
+        var names = new[] { "code-insiders", "code" };
+        var success = names.Select(name => Programs.Open(name, jArgs)).Any(x => x);
+        if (!success) {
+            Logger.Warn("Failed to open VSCode");
+            Environment.Exit(1);
+        }
+
+    }
+}
